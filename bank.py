55b8690abe70af2cee7c26358c9685253ea548b5
norahkibathi <norah.njeri@student.moringaschool.com>
2024-03-11 17:52:18 +0300

    trial

--- bank.py
+++ bank.py
@@ -1,27 +1,30 @@
 import ipdb
 import datetime
 import math
-def  solution (A,D):
-          
-      amount_balance = 0
-      
-      for i in range(len(A)):
-          amount_balance  = A[i]
-          date_x = D
-          transaction_cost = 0
-          start_date = datetime.date(2022,1,1)
-          end_date = datetime.date(2023,12,1)
-          month_date = math. int ("start_date - end_date")
-          if A[i] < 0:
-              if month_date == 3:
-               transaction_cost = 100 * 12
-               transaction_cost = D
-               print (f"Card Payment", "and", transaction_cost)
-                  
-          if A[i] > 0:
-               amount_balance +=  amount_balance - abs(5*12) 
-               date_x = datetime.datetime() 
-               print(f"Amount Balance",amount_balance , "the date of transaction is ", date_x)
-              
 
-ipdb.set_trace()               
+def solution(A, D):
+    amount_balance = 0
+
+    for i in range(len(A)):
+        amount_balance = A[i]
+        date_x = D[i]
+        transaction_cost = 0
+
+        start_date = datetime.date(2022, 1, 1)
+        end_date = datetime.date(2023, 12, 1)
+        month_duration = (end_date - start_date).date
+        month_duration = month_duration // 30  
+
+        if A[i] < 0:
+            if month_duration == 3:
+                transaction_cost = 100 * 12
+                print(f"Card Payment: {transaction_cost}")
+        elif A[i] > 0:
+            amount_balance += A[i] - abs(5 * 12)
+            print(f"Amount Balance: {amount_balance} on {date_x.strftime('%Y-%m-%d')}")
+
+
+A = [100, 100, 100, - 10]
+D = [datetime.date(2020, 12, 31), datetime.date(2020, 12, 22), datetime.date(2020, 12, 29), datetime.date(2020, 4, 1)]
+ipdb.set_trace()
+solution(A, D)
