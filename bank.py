d496862ecc28540bb9e3a52426d9764ac6b778ec
norahkibathi <norah.njeri@student.moringaschool.com>
2024-03-12 09:57:02 +0300

    at-least i tried

--- bank.py
+++ bank.py
@@ -1,4 +1,6 @@
 #calcuting the bank balances from amount balance , and transaction that is a product of months
+#A is an index representing the amount balance
+#D represents the date
 import ipdb
 import datetime
 import math
@@ -12,20 +14,23 @@ def solution(A, D):
         transaction_cost = 0
 
         start_date = datetime.date(2022, 1, 1)
-        end_date = datetime.date(2023, 12, 1)
+        end_date = datetime.date(2022, 12, 1)
         month_duration = (end_date - start_date).date
         month_duration = month_duration // 30  
-# the  loop to calculate the monthly amounts
+# the  loop to calculate the monthly amounts when generating the loop it rember we have cash and payment system
         if A[i] < 0:
             if month_duration == 3:
-                transaction_cost = 100 * 12
-                print(f"Card Payment: {transaction_cost}")
+               if A[i] < 0:
+                 transaction_cost = 100 * 12
+            print(f"Card Payment: {transaction_cost}")
         elif A[i] > 0:
             amount_balance += A[i] - abs(5 * 12)
-            print(f"Amount Balance: {amount_balance} on {date_x.strftime('%Y-%m-%d')}")
+            print(f"Amount Balance: {amount_balance} on {D[i].strftime('%Y-%m-%d')}")
 
-
-A = [100, 100, 100, - 10]
+#examples
+A = [100, 100, 100, -10]
 D = [datetime.date(2020, 12, 31), datetime.date(2020, 12, 22), datetime.date(2020, 12, 29), datetime.date(2020, 4, 1)]
+solution(A, D)
+
 ipdb.set_trace()
 solution(A, D)
